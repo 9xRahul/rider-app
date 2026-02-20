5d8725f3c31297887fd7e262c2d1d0b8bc86518f
9xrahul <meetmeerahul@gmail.com>
2026-02-20 19:19:49 +0530

    errors fixed

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -18,55 +18,75 @@ class _LOginScreenState extends State<LOginScreen> {
       builder: (context, state) {
         return state is AuthSuccessState
             ? HomeScreen()
-            : SingleChildScrollView(
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: <Color>[Colors.yellow, Colors.black],
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 200),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
+            : Scaffold(
+                body: SingleChildScrollView(
+                  child: BlocListener<AuthBloc, AuthState>(
+                    listener: (context, state) {
+                      if (state is AuthSuccessState) {
+                        ScaffoldMessenger.of(
+                          context,
+                        ).showSnackBar(SnackBar(content: Text(state.token)));
+                      }
 
-                      children: [
-                        SizedBox(height: 500),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 200),
-                          child: Center(
-                            child: GestureDetector(
-                              onTap: () {
-                                context.read<AuthBloc>().add(
-                                  LoginApiCallEvent(
-                                    username: "mor_2314",
-                                    password: "83r5^_",
+                      if (state is AuthFailureState) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(state.message),
+                            backgroundColor: Colors.red,
+                          ),
+                        );
+                      }
+                    },
+                    child: Container(
+                      height: MediaQuery.of(context).size.height,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: <Color>[Colors.yellow, Colors.black],
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 200),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+
+                          children: [
+                            SizedBox(height: 500),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 200),
+                              child: Center(
+                                child: GestureDetector(
+                                  onTap: () {
+                                    context.read<AuthBloc>().add(
+                                      LoginApiCallEvent(
+                                        username: "mor_2314",
+                                        password: "83r5^_",
+                                      ),
+                                    );
+                                  },
+
+                                  child: BlocBuilder<AuthBloc, AuthState>(
+                                    builder: (context, state) {
+                                      return state is AuthLoadingState
+                                          ? CircularProgressIndicator()
+                                          : CircleAvatar(
+                                              foregroundColor: Colors.white,
+                                              child: Center(
+                                                child: Icon(
+                                                  Icons.arrow_forward,
+                                                  color: Colors.black,
+                                                ),
+                                              ),
+                                            );
+                                    },
                                   ),
-                                );
-                              },
-
-                              child: BlocBuilder<AuthBloc, AuthState>(
-                                builder: (context, state) {
-                                  return state is AuthLoadingState
-                                      ? CircularProgressIndicator()
-                                      : CircleAvatar(
-                                          foregroundColor: Colors.white,
-                                          child: Center(
-                                            child: Icon(
-                                              Icons.arrow_forward,
-                                              color: Colors.black,
-                                            ),
-                                          ),
-                                        );
-                                },
+                                ),
                               ),
                             ),
-                          ),
+                          ],
                         ),
-                        if (state is AuthFailureState) Text("${state.message}"),
-                      ],
+                      ),
                     ),
                   ),
                 ),
